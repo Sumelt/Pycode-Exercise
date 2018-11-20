ed958e5d3b5a3b7ba42022d3177e7760059f1a57
Sumelt <40728261+Sumelt@users.noreply.github.com>
2018-11-20 23:45:13 +0800

    11/20

---
+++ remove space.py
@@ -0,0 +1,8 @@
+# @Author: Su
+# @Date: 2018-11-16 18:16:43
+# @Last Modified by:   Su
+# @Last Modified time: 2018-11-16 18:16:43
+# @Description: 删除字符串中所有的空格
+
+str = "	如 果 检 查 过 程 中 出 现 H(key)-k^2<0 的 情 况 （ 假 设 表 的 首 位 为 0 ），那 么 将 ( ( H(key)-k2 ) %TSize+ Tsize ) ％ TSize 作 为 结 果 （ 等 价 于 将 H(key)- k2 不 断 加 上 TSize 直 到 出 现 第 一 个 非 负 数 ）"
+print(str.replace(' ',''))
